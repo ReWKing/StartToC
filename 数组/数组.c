0ecf5a15dfadb6a95b579043ab023013b8a6a15a
ReWKing <baymaxing@gmail.com>
2018-10-12 11:03:06 +0800

    update

--- 数组/数组.c
+++ 数组/数组.c
@@ -1,23 +1,24 @@
 #include <stdio.h>
 
-//统计各个数字、空白符及其他字符出现的次数
+/*  统计各个数字、空白符及其他字符出现的次数  */
 main()
 {
-  int c, i, nwhite, nother;
-  int ndigit[10];
+    int c, i, nwhite, nother;
+    int ndigit[10];
 
-  nwhite = nother = 0;
-  for(i = 0; i< 10; i ++)
-    printf("%d %d %d\n", i, power(2, i), power(-3, i) );
-  return 0;
-}
-//power函数：求底数的n次幂；其中n >= 0
-int power(int base, int n)
-{
-  int i, p;
+    nwhite = nother = 0;
+    for (i = 0; i < 10; i++)
+        ndigit[i] = 0;
+    while ((c = getchar()) != EOF)
+        if (c >= '0' && c <= '9')
+            ++ndigit[c - '0'];
+        else if (c == ' ' || c == '\n' || c == '\t')
+            ++nwhite;
+        else
+            ++nother;
 
-  p = 1;
-  for (i = 1; i <= n; ++i)
-   p = p * base;
-return 0;
+    printf("digit =");
+    for (i = 0; i < 10; i++)
+         printf("%d", ndigit[i]);
+    printf(", white space = %d, other = %d\n", nwhite, nother);
 }
